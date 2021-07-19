aef9dd28d6be628efc42cd8c13d5caa3037fa906
David Paez <david.paez.ramirez98@gmail.com>
2021-07-19 12:54:20 -0500

    tests

---
+++ actions/action_test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 12:53:36 2021
+
+@author: david
+"""
+
---
+++ actions/utilities/utilitie_test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 12:53:49 2021
+
+@author: david
+"""
+
